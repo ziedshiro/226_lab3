71abb561dc9261993dabca25f0f9d04de5941c04
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 16:12:29 +0700

    6320502991_3_4

--- 6320502991_3_4.c
+++ 6320502991_3_4.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 int main()
 {
+    int i=3,n,N,k;
+    scanf("%d",&n);
+    for(N=2;N<=n;N++)
+    {
+        for(k=2;k<=i-1;k++)
+        {
+         if(i%k==0)
+         {
+             break;
+         }
+        }
+
+            printf("%d",k);
+
+        i++;
+    }
+
 
 }
