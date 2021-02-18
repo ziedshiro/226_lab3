cf6517dd30807673f2f4d5dde6afa57165b08cf3
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 14:57:48 +0700

    6320502991_3_2

--- 6320502991_3_2.c
+++ 6320502991_3_2.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main()
 {
-
+    int n;
+    scanf("%d",&n);
+    int i,j,N[n][2];
+    for(i=0;i<n;i++)
+        for(j=0;j<2;j++)
+        scanf("%d",&N[i][j]);
+    for(i=0;i<n;i++)
+        for(j=0;j<2;j++)
+            printf("%d",N[i][j]);
 }
