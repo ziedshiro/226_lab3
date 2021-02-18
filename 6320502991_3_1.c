6686961221366960cc0deab6789587a0e7dd8d76
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 14:47:29 +0700

    6320502991_3_1

--- 6320502991_3_1.c
+++ 6320502991_3_1.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 int main()
 {
-
+    int i,j,n[5][4];
+    for(i=0;i<5;i++)
+        for(j=0;j<4;j++)
+        scanf("%d",&n[i][j]);
+    for(i=0;i<5;i++)
+        for(j=0;j<4;j++)
+            printf("%d",n[i][j]);
 }
