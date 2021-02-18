9441918387f38a83d09a565d4fdea435003a3355
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 14:49:32 +0700

    6320502991_3_1

--- 6320502991_3_1.c
+++ 6320502991_3_1.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n[5][4];
+    int i,j,n[5][4],N[5],max=0,nm=0;
     for(i=0;i<5;i++)
         for(j=0;j<4;j++)
         scanf("%d",&n[i][j]);
     for(i=0;i<5;i++)
+            N[i]=0;
+    for(i=0;i<5;i++)
         for(j=0;j<4;j++)
-            printf("%d",n[i][j]);
+            N[i]+=n[i][j];
+          max=N[0];
+    for(i=0;i<5;i++)
+            if(max<N[i])
+            {
+                max=N[i];
+                nm=i;
+            }
+
+            printf("%d %d",nm+1,max);
 }
