6eb72fe9dc69da907ff1a54bfafe16e4839aa4e8
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 17:16:38 +0700

    6320502991_3_4

--- 6320502991_3_4.c
+++ 6320502991_3_4.c
@@ -1,25 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i=2,n,N,k;
+    int n,k=2;
     scanf("%d",&n);
-    int x[n+1];
-    for(N=2;N<=n;N++)
+     do
     {
-        for(k=2;k<=i;k++)
-         if(i%k==0)
-         {
-             break;
-         }
-        if(k==i)
-            x[N]=i;
-        i++;
-    }
-    if(x[n]==n)
-        printf("%d",n);
-    else if(n==1)
-        printf("1");
-    else
-        printf("%d",x[n-1]);
+        if(n%k==0)
+            n--;
+        else
+            k++;
+    }while(n!=k);
+    printf("%d",n);
 
 }
