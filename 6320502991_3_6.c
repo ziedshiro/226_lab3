b844f14e1a495bd56a1eb4be5addc76d2a9fc1dc
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 17:12:54 +0700

    6320502991_3_6

--- 6320502991_3_6.c
+++ 6320502991_3_6.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,i,j,k=2,x;
     scanf("%d",&n);
+    do
+    {
+        if(n%k==0)
+            n--;
+        else
+            k++;
+    }while(n!=k);
+    printf("%d",n);
+
 
 }
