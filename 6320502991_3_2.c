759f1d03e5543bf1fc18a756ce528a8be38e1edb
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 15:04:40 +0700

    6320502991_3_2

--- 6320502991_3_2.c
+++ 6320502991_3_2.c
@@ -3,11 +3,22 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int i,j,N[n][2];
+    int k,i,j,N[n][2],K[48],max=0,km=0;
+    for(i=0;i<48;i++)
+        K[i]=0;
     for(i=0;i<n;i++)
         for(j=0;j<2;j++)
         scanf("%d",&N[i][j]);
-    for(i=0;i<n;i++)
-        for(j=0;j<2;j++)
-            printf("%d",N[i][j]);
+    for(k=0;k<48;k++)
+        for(i=0;i<n;i++)
+                    if(N[i][0]==k)
+                      K[i]+=N[i][1];
+    max=K[0];
+    for(i=0;i<48;i++)
+        if(max<K[i])
+    {
+        max=K[i];
+        km=i;
+    }
+    printf("%d %d",km+1,K[i]);
 }
