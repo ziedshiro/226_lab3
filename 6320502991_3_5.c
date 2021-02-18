88e78d6450f8d38261719c9b032ea56f76301d05
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-18 16:45:16 +0700

    6320502991_3_5

--- 6320502991_3_5.c
+++ 6320502991_3_5.c
@@ -1,15 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int n,i=2,k;
-
+    int n,i=2,k=0,m,x=0;
     scanf("%d",&n);
+    m=n;
     do
     {
-        if(n%i==0)
+        if(m%i==0)
+        {
             k++;
+            m/=i;
+            if(k>1)
+                x=1;
+        }
         else
+        {
             i++;
-    }while(n>1);
+            k=0;
+        }
 
+    }while(m>1);
+    if(x==1)
+        printf("%d is not a Lucky Number.",n);
+    else
+        printf("%d is a Lucky Number.",n);
 }
